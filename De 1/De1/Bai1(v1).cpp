4993db8a5805641623c469cdb8cbb04e959dffab
t.trung2303@gmail.com <T.trung2303@gmail.com>
2023-08-27 21:36:10 +0700

    De 1

---
+++ De 1/De1/Bai1(v1).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+int main() {
+	ifstream input;
+	input.open("SQUARE.INP");
+	ofstream output;
+	output.open("SQUARE.OUT");
+	int n;
+	input >> n;
+	vector<pair<int, int>> vec;
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		input >> a >> b;
+		vec.push_back({ a,b });
+	}
+	int x_max = INT_MIN, x_min = INT_MAX, y_max = INT_MIN, y_min = INT_MAX;
+	for (int i = 0; i < n; i++) {
+		x_max = max(x_max, vec[i].first);
+		y_max = max(y_max, vec[i].second);
+		x_min = min(x_min, vec[i].first);
+		y_min = min(y_min, vec[i].second);
+	}
+	int canh = max(x_max - x_min, y_max - y_min);
+	int result = canh * canh;
+	output << result;
+	input.close();
+	output.close();
+	return 0;
+}
